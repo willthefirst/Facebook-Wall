e1ae5f131aa8bc9752102337b055a4c84f7479a1
Will Moritz <will@crowdfavorite.com>
2016-02-11 10:50:03 -0500

    better variables

--- js/wall.js
+++ js/wall.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
 	// 1: Select the button that the user clicks
-	$('??????').click(function() {
+	$('REPLACE_ME_WITH_A_SELECTOR').click(function() {
 		if ($('#message').val().trim() != "") {
 
 			var element = "<br/><div class='row'><div class='col-md-2'><img src='http://i.imgur.com/87kIXSN.jpg'></div>" +
 						  "<div class='col-md-10'>" + $('#message').val().trim() + "</div></div>";
 
 			// 2: Prepend the variable named "element" to the #wall div
-			$('#wall').??????(element);
+			$('#wall').REPLACE_ME_WITH_A_JQUERY_PREPEND_METHOD(element);
 
 			// 3: Change the background-color of each new facebook post
 
